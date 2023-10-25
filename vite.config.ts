771b2b58cfe40152066cb2a2e57a64d2264a2728
id88 <isisyouid@gmail.com>
2023-10-25 11:00:41 +0800

    Resolves the problem of incorrect icon path after building

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +16,12 @@ export default defineConfig({
             resolvers: [ElementPlusResolver()],
         }),
     ],
+    base: './',
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src"), //把 src 的别名设置为 @
+         },
+         // 类型： string[] 导入时想要省略的扩展名列表。
+         // extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.mjs']
+    }
 })
